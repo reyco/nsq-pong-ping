1a92ae75e95e0672a77d330728091b532c7f0fdc
Reyco Evangelista <clreyco@gmail.com>
2015-05-30 09:29:47 +0800

    partial

--- ping/ping.go
+++ ping/ping.go
@@ -8,58 +8,61 @@ import (
   "log"
 )
 
+
+type Essence struct{
+  config *Config
+  sender *Producer
+  receiver *Consumer
+}
+
 var(
   kaka = true
 )
 
-func send_ping(){
-  config := nsq.NewConfig()
-  w, _ := nsq.NewProducer("nsq:4150", config)
 
-  err := w.Publish("bing", []byte("Ping"))
+func (ess Essence) init(){
+  ess.config = nsq.NewConfig()
+  ess.sender, _ := nsq.NewProducer("nsq:4150", ess.config) 
+  ess.receiver, _ := nsq.NewConsumer("bong", "ch", config)
+}
+
+func (ess Essence) send_ping(){
+  err := ess.sender.Publish("bing", []byte("Ping"))
   if err != nil {
-      log.Panic("Could not connect wala")
+      log.Panic("Could not connect wala nada nothing")
   }
   fmt.Println("Ping is sent")
-  w.Stop()
-
 }
 
 
-func receive_pong(){
-  kaka = false
-  wg := &sync.WaitGroup{}
-  wg.Add(1)
-
-  config := nsq.NewConfig()
-  q, _ := nsq.NewConsumer("bong", "ch", config)
-  q.AddHandler(nsq.HandlerFunc(func(message *nsq.Message) error {
-      fmt.Println(string(message.Body), "is received")
-      // wg.Done()
-      send_ping()
-      kaka = true
-      return nil
-  }))
-  err := q.ConnectToNSQD("nsq:4150")
-  if err != nil {
-      log.Panic("Could not connect")
+func (ess Essence) receive_pong(){
+  if kaka{
+    kaka = false
+    ess.receiver.AddHandler(nsq.HandlerFunc(func(message *nsq.Message) error {
+        fmt.Println(string(message.Body), "is received")
+        ess.send_ping()
+        kaka = true
+        return nil
+    }))
+    err := ess.receiver.ConnectToNSQD("nsq:4150")
+    if err != nil {
+        log.Panic("Could not connect")
+    }
   }
-  // wg.Wait()
 }
 
 
 
 
 func main() {
-  send_ping()
-  tickChan := time.NewTicker(time.Millisecond * 400).C
+  ess := new(Essence)
+  ess.init()
+  ess.send_ping()
+  tickChan := time.NewTicker(time.Millisecond * 5000).C
   for {
     select{
       case <- tickChan:
-        if kaka{
-	  receive_pong()
-	  time.Sleep(time.Millisecond*5000)
-	}
+        ess.receive_pong()
     }
   }
 }
